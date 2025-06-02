1e5459bbd901de468982d30cbc9d9ed836324c5d
Rahul-Vishwa <rahulvishwa1403@gmail.com>
2025-06-02 14:15:14 +0530

    - Added SSR - fixed semantics and accessability properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { NgClass, ViewportScroller } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { isPlatformBrowser, NgClass, ViewportScroller } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, OnDestroy, OnInit, PLATFORM_ID, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 interface Form {
   name: FormControl<string | null>;
@@ -37,14 +38,36 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
     
   constructor(
     private scroller: ViewportScroller,
-    private http: HttpClient
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private titleService: Title,
+    private meta: Meta
   ) {}
 
   ngOnInit(): void {
-    Aos.init({
-      duration: 1000,
-      once: true
-    });
+    this.titleService.setTitle('Rahul Vishwakarma | Full Stack Developer | .NET | Angular | Healthcare');
+    this.meta.addTags([
+      {name: 'description', content: 'Rahul Vishwakarma - Full Stack Developer specializing in .NET and Angular for healthcare applications. Building scalable, secure, and efficient software solutions.'},
+      {name: 'keywords', content: 'Full Stack Developer, Rahul Vishwakarma, .NET Developer, Angular Developer, Healthcare Software, Web Developer, Software Engineer, Portfolio'},
+    
+      { property: 'og:title', content: 'Rahul Vishwakarma | Full Stack Developer | .NET | Angular | Healthcare' },
+      { property: 'og:description', content: 'Experienced Full Stack Developer focused on .NET and Angular technologies for healthcare software solutions.' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://yourdomain.com' },
+      { property: 'og:image', content: 'https://yourdomain.com/assets/profile-image.jpg' },
+      
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Rahul Vishwakarma | Full Stack Developer | .NET | Angular | Healthcare' },
+      { name: 'twitter:description', content: 'Experienced Full Stack Developer specializing in healthcare solutions using .NET and Angular.' },
+      { name: 'twitter:image', content: 'https://yourdomain.com/assets/profile-image.jpg' },
+    ]);
+
+    if (isPlatformBrowser(this.platformId)){
+      Aos.init({
+        duration: 1000,
+        once: true
+      });
+    }
 
     this.form = new FormGroup({
       name: new FormControl<string | null>(null, [Validators.required, Validators.minLength(3), Validators.maxLength(100), Validators.pattern(/^[a-zA-Z\s'-]+$/)]),
@@ -54,24 +77,25 @@ export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
   }
 
   ngAfterViewInit() {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const id = entry.target.getAttribute('id');
-            if (id) {
-              this.activeSection = id;
+    if (isPlatformBrowser(this.platformId)){
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              const id = entry.target.getAttribute('id');
+              if (id) {
+                this.activeSection = id;
+              }
             }
-          }
-        });
-      },
-      { threshold: 0.2 }
-    );
-
-
-    this.sections.forEach((section) => {
-      observer.observe(section.nativeElement);
-    });
+          });
+        },
+        { threshold: 0.2 }
+      );
+      
+      this.sections.forEach((section) => {
+        observer.observe(section.nativeElement);
+      });
+    }
   }
 
   scrollTo(anchor: string) {
